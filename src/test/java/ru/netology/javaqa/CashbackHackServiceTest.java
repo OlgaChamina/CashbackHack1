4e62c2852f1bb267ec44e6d496a15a651591f952
OlgaCh <chamina51@mail.ru>
2024-01-12 16:21:36 +0300

    initial commit

--- src/test/java/ru/netology/javaqa/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/javaqa/CashbackHackServiceTest.java
@@ -1,9 +1,9 @@
 package ru.netology.javaqa;
 
-import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     @Test
@@ -15,9 +15,9 @@ public class CashbackHackServiceTest {
         assertEquals (actual,expected);
     }
     @Test
-    public void calculateBonusIfAmount2000(){
+    public void calculateBonusIfAmount1000(){
         CashbackHackService service = new CashbackHackService();
-        int amount = 2000;
+        int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
         assertEquals (actual,expected);
